c01442007737b8195606ef6fa5c282a7de4d3495
Rui Lopes <rgl@ruilopes.com>
2020-05-21 21:26:09 +0100

    upgrade to packer-builder-arm-image 0.1.4.5 and packer 1.5.6

--- provision-packer.sh
+++ provision-packer.sh
@@ -9,14 +9,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 # install Packer.
 apt-get install -y unzip
-# NB packer-builder-arm-image is only compatible with packer 1.4.x.
-packer_version=1.4.5
+packer_version=1.5.6
 wget -q -O/tmp/packer_${packer_version}_linux_amd64.zip https://releases.hashicorp.com/packer/${packer_version}/packer_${packer_version}_linux_amd64.zip
 unzip /tmp/packer_${packer_version}_linux_amd64.zip -d /usr/local/bin
 
 # install the packer-builder-arm-image plugin.
 # see https://github.com/solo-io/packer-builder-arm-image
 apt-get install -y kpartx qemu-user-static
-wget -q -O/tmp/packer-builder-arm-image https://github.com/solo-io/packer-builder-arm-image/releases/download/v0.1.3/packer-builder-arm-image
+wget -q -O/tmp/packer-builder-arm-image https://github.com/solo-io/packer-builder-arm-image/releases/download/v0.1.4.5/packer-builder-arm-image
 install /tmp/packer-builder-arm-image -m 755 -C /usr/local/bin
 rm /tmp/packer-builder-arm-image
